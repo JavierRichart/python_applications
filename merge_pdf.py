5c741be66347c1fae717d3a13df9107a26ce9349
JavierRichart <lopezrichart@gmail.com>
2024-04-30 10:25:28 +0200

    firs commit

---
+++ merge_pdf.py
@@ -0,0 +1,13 @@
+from PyPDF4 import PdfFileMerger
+import os
+
+merger = PdfFileMerger()
+for items in os.listdir():
+    if items. endswith('.pdf'):
+        merger.append(items)
+    merger.write("final_pdf.pdf")
+    merger.close()
+
+for items in os.listdir():
+    if items != 'final_pdf.pdf' and items.endswith('.pdf'):
+        os.remove(items)
